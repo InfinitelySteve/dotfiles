ee2219a704b20b23e9f4a3ab6ffcb4d943aa7881
xxi <xxi@okami.local>
2020-12-04 00:23:38 -0500

    made cask script

---
+++ brewcask.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+brew install --cask sony-ps4-remote-play
+brew install --cask google-chrome
+brew install --cask google-photos-backup-and-sync
+brew install --cask parsec
+brew install --cask plex
+brew install --cask iterm2
+brew install --cask postman
+brew install --cask visual-studio-code
+brew install --cask discord
+brew install --cask iterm2
+brew install --cask sonos
+brew install --cask android-messages
+brew install --cask androidtool
+brew install --cask megasync
+brew install --cask vlc
+brew install --cask android-platform-tools
+brew install --cask cheatsheet
+brew install --cask moonlight
+brew install --cask spotify
+brew install --cask balenaetcher
+brew install --cask steam
