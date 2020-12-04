3f7d7034e5280ab8a4c9a1e95e7e27b9f7000f71
xxi <xxi@okami.local>
2020-12-04 07:29:44 -0500

    fix

--- brew.sh
+++ brew.sh
@@ -114,27 +114,6 @@ brew install youtube-dl
 brew install libusb
 brew install font-hack-nerd-font
 brew install font-powerline-symbols
-brew install --cask sony-ps4-remote-play
-brew install --cask google-chrome
-brew install --cask google-photos-backup-and-sync
-brew install --cask parsec
-brew install --cask plex
-brew install --cask iterm2
-brew install --cask postman
-brew install --cask visual-studio-code
-brew install --cask discord
-brew install --cask iterm2
-brew install --cask sonos
-brew install --cask android-messages
-brew install --cask androidtool
-brew install --cask megasync
-brew install --cask vlc
-brew install --cask android-platform-tools
-brew install --cask cheatsheet
-brew install --cask moonlight
-brew install --cask spotify
-brew install --cask balenaetcher
-brew install --cask steam
 
 # Begin install of Mac App store programs.
 mas install 1043270657 #GIF Keyboard (2.0.5)
